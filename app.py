11759ce6724631e020fb04018f1750dc8c6bea94
rv232r <rv232r@intl.att.com>
2018-10-08 15:22:10 +0200

    refactored few things, fixed some bugs, etc.

--- app.py
+++ app.py
@@ -7,15 +7,43 @@ import re
 
 
 
-def readlog(args, timestamp_from, timestamp_to):
+def readlog(args):
     # print(args, from_to_timestamp, end_to_timestamp)
-    stat=[]
+    stats=[]
+    if args.from_date is None:
+        args.from_date = "1970-01-01T00:00:00"
+        from_timestamp_set = False
+    if args.to_date is None:
+        args.to_date = "2070-01-01T00:00:00"
+        to_timestamp_set = False
+    if args.lazy is False:
+        check_date = re.compile("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}")
+        if check_date.match(args.from_date) is not None:
+            from_timestamp = pendulum.parse(args.from_date, strict=True).int_timestamp
+        else:
+            print("Starting date must be in ISO8601 format (YYYY-MM-DDThh:mm:ss)")
+            sys.exit()
+        if check_date.match(args.to_date) is not None:
+            to_timestamp = pendulum.parse(args.to_date, strict=True).int_timestamp
+        else:
+            print("Ending date must be in ISO8601 format (YYYY-MM-DDThh:mm:ss)")
+    else:
+        try:
+            from_timestamp = pendulum.parse(args.from_date, strict=False).int_timestamp
+            to_timestamp = pendulum.parse(args.to_date, strict=False).int_timestamp
+        except pendulum.parsing.exceptions.ParserError:
+            print("Cannot parse the date, try without the 'lazy' argument")
+    if from_timestamp > to_timestamp:
+        print("Starting date cannot be later than ending date. Maybe you confused '--from' and '--to'")
     for logfile in args.file:
         with open(logfile.name, 'r') as f:
             for line in f:
                 arr = line.split(",")
                 timestamp = arr[0]
-                if int(timestamp) >= timestamp_from and int(timestamp) <= timestamp_to:
+                if from_timestamp_set == False:
+                    from_timestamp = timestamp
+                    from_timestamp_set = True
+                if int(timestamp) >= from_timestamp and int(timestamp) <= to_timestamp:
                     found={}
                     found["timestamp"] = timestamp
                     found["http_host"] = arr[1]
@@ -24,27 +52,30 @@ def readlog(args, timestamp_from, timestamp_to):
                     found["duration"] = arr[4]
                     found["body_size"] = arr[5]
                     found["ip_address"] = arr[6]
-                    stat.append(found)
-    return stat
+                    stats.append(found)
+            if to_timestamp_set == False:
+                to_timestamp = timestamp
+                to_timestamp_set = True        
+    return stats
 
 
-def create_stat(stat):
+def create_stat(stats):
     status_2xx = re.compile("2..")
     status_3xx = re.compile("3..")
     status_4xx = re.compile("4..")
     status_5xx = re.compile("5..")
     api_sum_2xx, api_sum_3xx, api_sum_4xx, api_sum_5xx = 0,0,0,0
     tools_sum_2xx, tools_sum_3xx, tools_sum_4xx, tools_sum_5xx = 0,0,0,0
-    for item in stat:
+    for item in stats:
         if item["http_host"] == "api":
             if status_2xx.match(item["http_status"]) is not None:
                 api_sum_2xx+=1
             elif status_3xx.match(item["http_status"]) is not None:
                 api_sum_3xx+=1
             elif status_4xx.match(item["http_status"]) is not None:
-                api_sum_4xx=1
+                api_sum_4xx+=1
             elif status_5xx.match(item["http_status"]) is not None:
-                api_sum_5xx=1
+                api_sum_5xx+=1
         elif item["http_host"] == "tools":
             if status_2xx.match(item["http_status"]) is not None:
                 tools_sum_2xx+=1
@@ -82,34 +113,11 @@ def get_stat():
     parser.add_argument("file", type=argparse.FileType('r'), nargs='+', help="file or files contain the logs")
     parser.add_argument("--from", dest="from_date", help="starting date in ISO8601 format (YYYY-MM-DDThh:mm:ss)")
     parser.add_argument("--to", dest="to_date", help="ending date in ISO8601 format (YYYY-MM-DDThh:mm:ss)")
-    parser.add_argument("--lazy", "-l", dest="lazy", action="store_true", help="lazy mode - you can omit everything except year")
+    parser.add_argument("--lazy", "-l", dest="lazy", action="store_true", help="lazy mode - you can input only partial date (for example: '1975-12-25'), or you can change the format (for example: '25-Dec-1975 14:15:16' instead of '1975-12-25T14:15:16'), pendulum is going to try to parse it.")
     args = parser.parse_args()
-    if args.from_date is None:
-        args.from_date = "1970-01-01T00:00:00" # If we just want to test without giving from-to
-    if args.to_date is None:
-        args.to_date = "2070-01-01T00:00:00" # If we just want to test without giving from-to
-    if args.lazy is False:
-        check_date = re.compile("\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}")
-        if check_date.match(args.from_date) is not None:
-            from_to_timestamp = pendulum.parse(args.from_date, strict=True).int_timestamp
-        else:
-            print("Starting date must be in ISO8601 format (YYYY-MM-DDThh:mm:ss)")
-            sys.exit()
-        if check_date.match(args.to_date) is not None:
-            end_to_timestamp = pendulum.parse(args.to_date, strict=True).int_timestamp
-        else:
-            print("Ending date must be in ISO8601 format (YYYY-MM-DDThh:mm:ss)")
-    else:
-        try:
-            from_to_timestamp = pendulum.parse(args.from_date, strict=False).int_timestamp
-            end_to_timestamp = pendulum.parse(args.to_date, strict=False).int_timestamp
-        except pendulum.parsing.exceptions.ParserError:
-            print("Cannot parse the date, try without the 'lazy' argument")
-    if from_to_timestamp > end_to_timestamp:
-        print("Starting date cannot be later than ending date. Maybe you confused '--from' and '--to'")
-    stat = readlog(args, from_to_timestamp, end_to_timestamp)
+    stats = readlog(args)
     # print(stat)
-    result = create_stat(stat)
+    result = create_stat(stats)
     # print(result)
     print("""
     Betweeen time {} and {}:
